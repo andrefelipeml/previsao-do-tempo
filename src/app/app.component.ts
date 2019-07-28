b59553b581099a10096bcd76f03b3c25391bad2d
André Leite <andref.moreiraleite@gmail.com>
2019-07-27 21:59:14 -0300

    corrigido alinhamento e implementado loading e favoritar cidade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,50 +22,56 @@ export class AppComponent implements OnInit  {
   faCloudSun = faCloudSun;
   states: State;
   cities: City;
-  citySelected: City;
+  citySelected: any;
+  stateSelected: any;
+  public loading: boolean;
+  forecastNextDays = new Array();
 
   constructor(private climaTempoService: ClimaTempoService) {
+    this.loading = true;
     this.climaTempoService.getStates().subscribe(x => {
       console.log('States', x);
       this.states = x;
+      const state = localStorage.getItem('state');
+      const city = localStorage.getItem('city');
+      if (state && city) {
+        this.stateSelected = state;
+        this.getCities(state);
+        this.citySelected = city;
+      } else {
+        this.getCities('SC');
+        localStorage.setItem('state', 'SC');
+        localStorage.setItem('city', 'Blumenau');
+      }
+      this.getInformations();
+      this.loading = false;
     });
 
   }
 
-  ngOnInit(): void {
-    setTimeout(() => {
-      const dataa = [
-      { day: '26/07/2019', min: 20, max: 25 },
-      { day: '27/07/2019', min: 14, max: 20 },
-      { day: '28/07/2019', min: 18, max: 22 },
-      { day: '29/07/2019', min: 12, max: 18 },
-      { day: '30/07/2019', min: 13, max: 17  }];
-      Morris.Line({
-        element: 'morris-extra-area',
-        data: dataa,
-        // The name of the data record attribute that contains x-values.
-        xkey: 'day',
-        // A list of names of data record attributes that contain y-values.
-        ykeys: ['min', 'max'],
-        // Labels for the ykeys -- will be displayed when you hover over the
-        // chart.
-        labels: ['Minimo', 'Maximo']
-      });
-    });
-  }
+  ngOnInit(): void { }
 
-  getCities(id: string) {
+ async getCities(id: string) {
+    this.loading = true;
     this.climaTempoService.getCities(id).subscribe(x => {
       this.cities = x;
+      this.loading = false;
     });
   }
 
-  getInformations(i) {
-    this.climaTempoService.getCityId(this.cities[i]).subscribe(city => {
-      console.log('aaaaaaaaaaaaa', city);
-      this.climaTempoService.get(city[0]).subscribe(x => {
+  getInformations() {
+    this.loading = true;
+    this.climaTempoService.getCityId(this.stateSelected, this.citySelected).subscribe(city => {
+      this.climaTempoService.get(city[0].id).subscribe(x => {
         this.response = x;
+        this.loading = false;
+        this.forecastNextDays = x['data'].splice(1, 5);
       });
     });
+  }
+
+  favoriteCity(){
+    localStorage.setItem('state', this.stateSelected);
+    localStorage.setItem('city', this.citySelected);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +15,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [ClimaTempoService],
   bootstrap: [AppComponent]
